334715f4098bba03888ab563abf54d0640ab582c
Thamsanqa Shabalala <thammy202@gmail.com>
2020-11-05 13:20:06 +0200

    user release page

--- app/src/main/java/com/jukebox/world/MainActivity.java
+++ app/src/main/java/com/jukebox/world/MainActivity.java
@@ -3,8 +3,19 @@ package com.jukebox.world;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,9 +25,17 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mCustomerDatabase;
+    private String userID;
+
+    private TextView mprofileName;
+    private ImageView mProfileImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +43,10 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mAuth = FirebaseAuth.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+        mCustomerDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(userID);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -35,6 +58,14 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         NavigationUI.setupWithNavController(navigationView, navController);
 
 
+        View header = navigationView.getHeaderView(0);
+
+        mProfileImage = header.findViewById(R.id.profileImage);
+        mprofileName = header.findViewById(R.id.profileName);
+
+        getUserInfo();
+
+
         /*BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -42,6 +73,33 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         NavController navControllerBottom = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navControllerBottom, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navControllerBottom);*/
+    }
+
+    private void getUserInfo() {
+        mCustomerDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+
+                        /*if (map.get("firstName") != null &&  map.get("lastName") != null) {
+                            mprofileName.setText(map.get("name").toString().trim() + " " + map.get("lastName").toString().trim());
+                        }*/
+
+                        if (map.get("firstName") != null) {
+                            mprofileName.setText(map.get("firstName").toString().trim());
+                        }
+
+                        if (map.get("profileImageUrl") != null) {
+                            Glide.with(getApplication()).load(map.get("profileImageUrl").toString().trim()).into(mProfileImage);
+                        }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
     @Override
